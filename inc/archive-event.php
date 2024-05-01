3450bba7669f7089c9da8328920d4854ec4b45b2
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-05-01 00:43:23 -0400

    Add files via upload

--- inc/archive-event.php
+++ inc/archive-event.php
@@ -1,34 +1,18 @@
-<?php get_header(); 
-$pastEvents = new WP_Query(array(
-    'post_type' => 'event',
-    'paged' => get_query_var('paged', 1),
-    'orderby' => 'meta_value',
-    'meta_query' => array(
-        array(
-          'key' => 'event_date',
-        )
-        ),
-    'meta_type' => 'DATE',
-    'order' => 'ASC'
-  ));
+<?php get_header();
 
-?><div class="banner">
-    <h1 class="centered-text">Events</h1>
-</div>
-<div class="two-thirds-screen">
-    <?php while(have_posts()){
-        the_post(); ?>
-        <div class="accent-wrapper-1">
-            <div class="accent-wrapper-0">                    
-                <div class="archive-item">
-                    <h2><a class="gray-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <p><?php echo get_field('event_date'); ?> EST</p>
-                    <?php the_excerpt(); ?>
-                    <p><a href="<?php the_permalink(); ?>">See more &raquo;</a></p>
-                </div>
+?><main>
+    <div class="banner"><h1 class="centered-text">Events</h1></div>
+    <br>
+    <br>
+    <div class="generic-wrapper-2">
+        <div class="generic-wrapper-1">
+            <div class="generic-wrapper-0">
+                <h2 class="centered-text">Coming soon!</h2>
+                <p>We're excited for members to share events with each other.</p>
             </div>
         </div>
-    <?php }
-?></div>
+    </div>
+</main>
 
-<?php get_footer(); ?>
+<?php get_footer();
+?>
--- src/modules/Event-Form.js
+++ src/modules/Event-Form.js
@@ -2,10 +2,16 @@ import $ from 'jquery';
 
 class EventForms{
     constructor(){
+        //Add a new event---------------------------------------------------------------------------
         this.submitNewEventButton = $('#tomc--save-new-event');
         this.cancelNewEventButton = $('#tomc--cancel-new-event');
         this.newEventNameInput = $('#tomc-book-organization-new-event');
         this.newEventTextarea = $('#tomc-book-organization--new-event-details');
+        this.displayEventTime = $('#tomc-events-time-entered');
+        this.eventTimePicker = $('#tomc-event-date-time-picker');
+        this.presenterSelect = $('#tomc-event-presenter-select');
+        this.addPresenterButton = $('#tomc-event-attendance-add-presenter');
+        this.eventPresenterContainer = $('#tomc-event-presentation-container');
         this.events();
     }
 
